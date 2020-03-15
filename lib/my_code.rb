e154074031be2f454869a19ecd7ea1d5562a08b4
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-15 21:42:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce(source_array, starting_value=0)
   value = starting_value
   index = 0
   while index < source_array.length do 
-    value = yield(source_array[index], value)
+    value = yield(value, source_array[index])
     index+=1 
   end 
   puts value
